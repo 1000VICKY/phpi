0595b2f1528e2f77d5f4b11daf46b72ee75ad869
senbiki <aki.sen.1209@gmail.com>
2019-04-07 22:02:15 +0900

    echo/echo.go

--- echo/echo.go
+++ echo/echo.go
@@ -1,11 +1,19 @@
+// 簡易に標準出力に出力する関数を定義する
 package echo;
 
 import "os";
-// echo関数を定義
-var Echo func (string) (int, error) = func (s string) (int, error) {
-    // os.Stdout.Writeメソッドに渡す文字列を[]byteへ変換
+
+/**
+ * Echo 関数を定義する
+ * @param s string
+ * @return int, error
+ */
+func Echo (s string ) (int, error) {
+    // 文字列をバイト列化
     var buffer []byte = []byte(s);
-    size, err := os.Stdout.Write(buffer);
-    // Writeメソッドの戻り値をそのまま返却
-    return size, err;
-};
+    var size *int = new(int);
+    var err error = nil;
+    *size, err = os.Stdout.Write(buffer);
+    // 上記戻り値をそのまま返却
+    return *size, err;
+}
