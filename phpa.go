e6e92d219cf4971ad23c68ba03c2cf2b0ae3392d
千疋　哲史 <senbiki@c-shop.net>
2019-04-11 16:42:48 +0900

    PHPスクリプトの初期化不具合を修正

--- phpa.go
+++ phpa.go
@@ -68,7 +68,7 @@ func main() {
 
 	// 実行するPHPスクリプトの初期化
 	// バックティックでヒアドキュメント
-	const initializer = "<?php" +
+	const initializer = "<?php " +
 		"ini_set(\"display_errors\", 1);" +
 		"ini_set(\"error_reporting\", -1);";
 
--- phpa_unix.go
+++ phpa_unix.go
@@ -71,7 +71,7 @@ func main() {
 
 	// 実行するPHPスクリプトの初期化
 	// バックティックでヒアドキュメント
-	const initializer = "<?php" +
+	const initializer = "<?php " +
 		"ini_set(\"display_errors\", 1);" +
 		"ini_set(\"error_reporting\", -1);";
 
